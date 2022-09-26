c7e4edd7b52ea3eb54792e065e7dbe7882c88220
MulindwaDenis13 <mulindwadenis13@gmail.com>
2022-09-26 13:56:34 +0300

    resolve issues

--- resources/views/backend/product/product_add.blade.php
+++ resources/views/backend/product/product_add.blade.php
@@ -295,7 +295,7 @@
 			<div class="col-md-4">
 
 	    <div class="form-group">
-			<h5>Product Discount Price <span class="text-dangre">*</span></h5>
+			<h5>Product Discount Price <span class="text-danger">*</span></h5>
 			<div class="controls">
 	 <input type="text" name="discount_price" class="form-control" required=''>
      @error('discount_price')
